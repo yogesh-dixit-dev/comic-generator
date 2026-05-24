4a5537aa0ba16e9ef17faf1088387e9f1dc2885c
november-services <November.services25@gmail.com>
2026-05-24 16:06:41 +0530

    Add vercel config

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
